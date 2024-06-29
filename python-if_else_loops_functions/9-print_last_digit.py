7ec4f90bf468ff1906e137a9732a9fad15a16aff
admirec <97503470+octaviusthe3rd@users.noreply.github.com>
2024-06-29 14:51:09 +0200

    fix#3

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            l = ord(i) - 32
-        else:
-            l = ord(i)
-        print("{:c}".format(l), end="")
-    print("")
+def print_last_digit(number):
+    if number < 0:
+        number = abs(number)
+    print("{:d}".format(number % 10), end="")
+    return number % 10
