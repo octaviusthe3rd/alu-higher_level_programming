3909d8e5ef6d43e4172920f19662415dadcec64a
octaviusthe3rd <admirechaga@gmail.com>
2024-07-13 05:44:49 -0700

    task#2

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define a class
+"""
+
+
+class Square:
+    """
+    Class creates a private size instance variable and assisgns it
+    """
+    def __init__(self, size):
+        self._Square__size = size
