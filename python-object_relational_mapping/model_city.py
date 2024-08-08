904567b0afe8ef3ce2c641f1c14074f5b0f946f4
octaviusthe3rd <admirechaga@gmail.com>
2024-08-08 16:01:55 -0700

    task#15

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creates a class city"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """class city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
