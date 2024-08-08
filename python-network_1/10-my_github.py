c83ee8a70ed1aaf17efff81f14ecd949320b4d82
octaviusthe3rd <admirechaga@gmail.com>
2024-08-08 12:51:51 -0700

    task#2: 10-my_github

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python script takes Github login credentials and uses the Github API to display id"""
+
+import requests
+from sys import argv
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    r = requests.get('https://api.github.com/user',
+                     params={"login": username},
+                     auth=(username, password)).json()
+    if "id" in r:
+        print(r["id"])
+    else:
+        print(None)
