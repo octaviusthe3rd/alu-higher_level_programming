2c6f272a7305bb36627cf12fa2748eeb2648a472
octaviusthe3rd <admirechaga@gmail.com>
2024-08-08 15:21:40 -0700

    fix#1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,7 +4,6 @@ import json
 import csv
 from random import choice as random
 
-
 class Base:
     """ this is a base class that keeps track of ids"""
     id = 0
@@ -19,14 +18,12 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        
         if list_dictionaries is None:
             list_dictionaries = []
         return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-    
         lst = []
         if list_objs is not None and len(list_objs) > 0:
             for obj in list_objs:
