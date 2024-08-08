395ea0327f4938151718fd237aed6e02d5215de4
octaviusthe3rd <admirechaga@gmail.com>
2024-08-08 12:52:15 -0700

    task#7: 5-hbtn_header

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that sends a request to the URL and displays the value of
+X-Request-Id in the response header"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    req = requests.get(sys.argv[1])
+    print(req.headers.get("X-Request-Id"))
