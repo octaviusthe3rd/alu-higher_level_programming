b9e7fda78918bfda2f9c4cc3a9bdb8566804ff7d
octaviusthe3rd <admirechaga@gmail.com>
2024-08-08 13:00:04 -0700

    fix#4

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Python script takes Github login credentials 
--and uses the Github API to display id"""
+"""
+Python script takes Github login credentials and uses the Github API to display id
+"""
 import requests
 from sys import argv
 if __name__ == "__main__":
