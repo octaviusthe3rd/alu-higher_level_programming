730c3164acba3218968a7eccf8e64cf2164a1379
octaviusthe3rd <admirechaga@gmail.com>
2024-09-27 06:22:44 -0700

    task#2

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let firstLine = 'C is fun';
 let secondLine = 'Python is cool';
-let thirdLine = 'Javascript is amazing';
+let thirdLine = 'JavaScript is amazing';
 console.log(`${firstLine}\n${secondLine}\n${thirdLine}`);
