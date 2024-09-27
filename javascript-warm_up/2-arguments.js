ee7b31842dc8d62003ee6d94ed4cc713cd2060f4
octaviusthe3rd <admirechaga@gmail.com>
2024-09-27 10:12:18 -0700

    task#3

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arguments = process.argv.slice(2)
+if(arguments.length === 1 ){
+console.log('Argument found')
+}else if(arguments.length > 1){
+console.log('Arguments found')
+}else{
+console.log('No arguments found')
+}
