c1095c0d64572381a68ed72de652182272feac69
octaviusthe3rd <admirechaga@gmail.com>
2024-09-27 12:25:13 -0700

    fix#5

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (arguments[0]) {
-  console.log(arguments[0]);
+if (args[0]) {
+  console.log(args[0]);
 } else {
   console.log('No argument');
 }
