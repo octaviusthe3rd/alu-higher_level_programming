0ad29a8b750c59d5619d15e22765325a9996b13a
octaviusthe3rd <admirechaga@gmail.com>
2024-09-27 12:22:59 -0700

    fix#4

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args.length === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else if (args.length > 1) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
-
