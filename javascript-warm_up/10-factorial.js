47f2fbad24da9f52121f69815296a3906f6021ae
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 10:13:20 -0700

    task#11

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const number = parseInt(process.argv[2]);
+
+function factorial(i) {
+	if (isNaN(i)) {
+		return 1;
+	}else if (i <= 1){
+		return 1;
+	}else{
+	return i * factorial(i - 1);
+	}
+}
+
+console.log(factorial(number));
