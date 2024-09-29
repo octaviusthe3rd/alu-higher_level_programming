97f0101bd82f16f28ee9f4083c83cb650465e43d
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 12:49:40 -0700

    fix#4

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,13 +3,14 @@
 const number = parseInt(process.argv[2]);
 
 function factorial(i) {
-	if (isNaN(i)) {
-		return 1;
-	}else if (i <= 1){
-		return 1;
-	}else{
-	return i * factorial(i - 1);
-	}
+  if (isNaN(i)) {
+    return 1;
+  } else if (i <= 1) {
+    return 1;
+  } else {
+    return i * factorial(i - 1);
+  }
 }
 
 console.log(factorial(number));
+
