2c401d5733269bcc6096f0af9fc90e31416ee1af
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 09:57:23 -0700

    task#10

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+function add(a, b) {
+	return a + b;
+}
+
+
+if (!isNaN(firstInt) && !isNaN(secondInt)) {
+	let sum = add(firstInt, secondInt);
+	console.log(`The sum is ${sum}.`);
+}else{
+	console.log('Invalid arguments provided. Make sure you to provide only two integer arguments.');
+}
