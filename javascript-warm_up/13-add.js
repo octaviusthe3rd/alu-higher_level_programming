bef42d0e14f58b303b5a24d4dec408c829532c32
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 11:21:19 -0700

    fix#2

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const add = require('./add').add;
 
-console.log(add(4, 85));
-console.log(add(93, -4));
-console.log(add(0, 89));
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { add };
