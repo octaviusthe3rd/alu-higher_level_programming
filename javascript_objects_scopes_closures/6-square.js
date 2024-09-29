f944874744991ba4dd6f0ca1719e646a9146c02e
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 12:00:53 -0700

    fix#3

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
   }
 };
