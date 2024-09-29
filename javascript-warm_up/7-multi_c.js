e1a96110c722c9b58a764eaf13cc8905686ceccf
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 09:23:06 -0700

    task#8

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,6 +2,10 @@
 const userInput = parseInt(process.argv[2]);
 const output = 'C is fun'
 
-for (let i = 0; i < userInput; i++) {
-console.log(output);
+if(!isNaN(userInput)) {
+	for (let i = 0; i < userInput; i++) {
+		console.log(output);
+	}
+}else{
+	console.log('Missing number of occurrences');
 }
