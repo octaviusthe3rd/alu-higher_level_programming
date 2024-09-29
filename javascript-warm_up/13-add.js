74e7207c90accd0285379c7af148ff3e62415d31
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 10:46:32 -0700

    fix#1

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const add = require('./add').add;
-console.log(add(3, 5));
+const add = require('./13-add').add;
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (!isNaN(a) && !isNaN(b)) {
+  console.log(add(a, b));
+} else {
+  console.log('Please provide two valid integers as arguments');
+}
