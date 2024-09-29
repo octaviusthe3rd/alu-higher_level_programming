a5f6eb38f8c1a82fc2ce0674b67caa373be154a5
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 10:51:46 -0700

    fix#2

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -9,7 +9,7 @@ function add(a, b) {
 
 if (!isNaN(firstInt) && !isNaN(secondInt)) {
 	let sum = add(firstInt, secondInt);
-	console.log(`The sum is ${sum}.`);
+	console.log(sum);
 }else{
 	console.log('Invalid arguments provided. Make sure you to provide only two integer arguments.');
 }
