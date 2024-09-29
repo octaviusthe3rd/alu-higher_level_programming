0cd15fee0ae83efa2fd0bec2a0d6553a74aa6363
octaviusthe3rd <admirechaga@gmail.com>
2024-09-29 10:27:14 -0700

    task#12

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const arg = process.argv.slice(2);
+
+if (arg.length <= 1) {
+    console.log(0);
+} else {
+    const numbers = arg.map(Number);
+    let biggest = numbers[0];
+    let secondBiggest = 0;
+
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > biggest) {
+            secondBiggest = biggest;
+            biggest = numbers[i];
+        } else if (numbers[i] > secondBiggest && numbers[i] < biggest) {
+            secondBiggest = numbers[i];
+        }
+    }
+
+    console.log(secondBiggest);
+}
