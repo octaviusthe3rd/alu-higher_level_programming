98ea4d3d2a9bcda81cf6a7170463af5511d8bdeb
octaviusthe3rd <admirechaga@gmail.com>
2024-10-05 08:57:48 -0700

    fix#1

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 const arg = process.argv[2];
 
-fs.readFile(arg, 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(arg, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
