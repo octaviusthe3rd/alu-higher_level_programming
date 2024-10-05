1179372a68aec92645513cf220f0a0d62e38eedb
octaviusthe3rd <admirechaga@gmail.com>
2024-10-05 11:15:53 -0700

    task#5

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    try {
+      const results = JSON.parse(body).results || [];
+      const count = results.filter(movie => 
+        movie.characters.some(character => character.endsWith('/18/'))
+      ).length;
+      console.log(count);
+    } catch (parseError) {
+      console.error('Error parsing response:', parseError);
+    }
+  }
+});
