0b37c6b5811e224c549721751115a0d1b65f5a5c
octaviusthe3rd <admirechaga@gmail.com>
2024-10-05 09:15:33 -0700

    task#2

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(filePath, string,  error => {
-  if (error) console.log(error);
+fs.writeFile(filePath, string, (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
+
