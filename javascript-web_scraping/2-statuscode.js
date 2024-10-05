511b1714bc78d1a55f7e84c1b5947dbb6d74adfd
octaviusthe3rd <admirechaga@gmail.com>
2024-10-05 09:47:05 -0700

    task#3

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,10 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function (err, response, body){
-	if (err) {
-		console.log(err);
-	}else {
-		console.log("code: " + response.statusCode);
-	};
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
